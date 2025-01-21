62bd5c5cc62b39ad776f7101ff48569e8aff8464
katielizinha <116743082+katielizinha@users.noreply.github.com>
2025-01-21 11:10:51 -0400

    commit

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-//LISTAR
+
 app.get("/produtos", async (req, res) => {
     try {
         const banco = new BancoMongo()
@@ -21,7 +21,19 @@ app.get("/produtos", async (req, res) => {
         res.status(500).send("Server ERROR")
     }
 })
-//iNSERIR
+app.get("/produtos/:id", async (req, res) => {
+    try {
+        
+        const banco = new BancoMongo()
+        await banco.criarConexao()
+        const result = await banco.listarPorId(req.params.id)
+        await banco.finalizarConexao()
+        res.send(result)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send("Server ERROR")
+    }
+})
 app.post("/produtos", async (req, res) => {
     try {
         const {id,nome,descricao,preco,imagem} = req.body
@@ -30,7 +42,7 @@ app.post("/produtos", async (req, res) => {
         const produto = {id,nome,descricao,preco,imagem}
         const result = await banco.inserir(produto)
         await banco.finalizarConexao()
-        res.send(result)
+        res.send(result) 
     } catch (e) {
         console.log(e)
         res.status(500).send(e)
@@ -44,21 +56,22 @@ app.delete("/produtos/:id",async(req,res)=>{
         await banco.criarConexao()
         const result = await banco.excluir(req.params.id)
         await banco.finalizarConexao()
-        res.send("Produto excluido com sucesso id: "+req.params.id)
+        res.status(200).send("Produto excluido com sucesso id: "+req.params.id)
     }
-   catch(e){
-    console.log(e)
-    res.status(500).send("Erro ao excluir")
-   }
+    catch(e){
+        console.log(e)
+        res.status(500).send("Erro ao excluir")
+    }
+    
 })
 
 //ALTERAR
 app.put("/produtos/:id",async(req,res)=>{
-    const {id,nome,descricao,preco,imagem} = req.body
+    const {nome,descricao,preco,imagem} = req.body
     const produto = {nome,descricao,preco,imagem}
     const banco = new BancoMongo()
     await banco.criarConexao()
-    const result = await banco.alterar(req.params.id, produto)
+    const result = await banco.alterar(req.params.id,produto)
     await banco.finalizarConexao()
     res.status(200).send("Produto alterado com sucesso id: "+req.params.id)
 })
